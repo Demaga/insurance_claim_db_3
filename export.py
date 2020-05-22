c9fe82be1b393f8ea9e9cd199898baefda265fa3
Bogdan Kholodenko <Demaga1234@gmail.com>
2020-05-22 14:30:45 +0300

    Update export.py

--- export.py
+++ export.py
@@ -1,5 +1,5 @@
-import csv
 import cx_Oracle
+import csv
 
 dsn_tns = cx_Oracle.makedsn('DESKTOP-QCKCHB8', '1521', service_name='XE')
 connection = cx_Oracle.connect(user='Demaga', password='31415Bog', dsn=dsn_tns)
@@ -8,16 +8,16 @@ cursor = connection.cursor()
 
 tables = ["customer", "education", "incident", "policy", "collision_type", "state", "incident_type"]
 
-for t in tables:
-    file = t + ".csv"
-    query = "SELECT * FROM {}".format(t)
+for table in tables:
+    file = table + ".csv"
+    query = "SELECT * FROM {}".format(table)
     cursor.execute(query)
     with open(file, "w", encoding = "utf-8") as csv_file:
         writer = csv.writer(csv_file, delimiter = ",")
-        r = cursor.fetchone()
-        while r != None:
-            writer.writerow(list(r))
-            r = cursor.fetchone()
+        row = cursor.fetchone()
+        while row != None:
+            writer.writerow(list(row))
+            row = cursor.fetchone()
 
 cursor.close()
 connection.commit()
